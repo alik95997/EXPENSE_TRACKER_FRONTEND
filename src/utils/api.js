b05fac150e7701a3c065b4aa61fd6f052283b36b
Ali Khan <alik95997@gmail.com>
2026-01-15 22:11:48 +0500

    fixed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-// Create axios instance with default configuration
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
-  withCredentials: true, // Send cookies with every request
+  withCredentials: true, 
   headers: {
     "Content-Type": "application/json",
   },
